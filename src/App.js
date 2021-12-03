f2bebaabe2d12708453e5aca4a05ce0c2bf94a17
darlin74 <dpacheosuarez2138@rihmondhillhs.org>
2021-12-03 16:49:00 +0000

    furbook profile

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import PetNav from './components/pet-nav'
 import PictureBox from './components/picture-box'
+import AboutBox from './components/about-box'
+import Post from './components/post'
 import './App.css';
 
 const App = () => {
@@ -10,12 +12,15 @@ const App = () => {
     return(
       <div className="App">
         <PetNav/>
+       
         <div class="container">
           <div class="row">
-            //Add profile picture and about section here
+          <PictureBox/>
+          <AboutBox/>
+        
           </div>
           <div class="row">
-            //Add posts here
+        <Post/>
           </div>
         </div>
       </div>
